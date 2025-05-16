44209168224f40db2738d2a9291340d32d1d54c7
Esther-Sonia <esthersonia21@gmail.com>
2025-05-16 10:22:50 +0300

    finish lab on one to many object relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,37 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        pets_list = []
+        for pet in Pet.all:
+            if pet.owner == self:
+                pets_list.append(pet)
+        return pets_list
+
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception("The pet must be an instance of the Pet class")
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("The owner must be an instance of the Owner class")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        Pet.all.append(self)
